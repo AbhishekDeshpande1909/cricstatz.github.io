af57267d1345ee38c8e8bf1e035331bd9e1d711b
abhishek <abhi.d010@gmail.com>
2020-06-02 09:55:46 +0530

    Cricket Tournament Managment System

---
+++ Tourn.js
@@ -0,0 +1,155 @@
+function changeresults()
+{
+    document.getElementById("changeResult").style.visibility="visible";
+    document.getElementById("changeFixture").style.visibility="hidden";
+    document.getElementById("changeStatz").style.visibility="hidden";
+    document.getElementById("10v").style.visibility="hidden";
+    document.getElementById("5v").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="hidden";
+    document.getElementById("TopWicket").style.visibility="hidden";   
+    document.getElementById("BatsmenTop").style.visibility="hidden";
+    document.getElementById("BatSR").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="hidden";
+    document.getElementById("BatAVG").style.visibility="hidden"; 
+}
+function changefixtures()
+{
+    document.getElementById("changeFixture").style.visibility="visible";
+    document.getElementById("changeResult").style.visibility="hidden";
+    document.getElementById("changeStatz").style.visibility="hidden";
+    document.getElementById("10v").style.visibility="hidden";
+    document.getElementById("5v").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="hidden";
+    document.getElementById("TopWicket").style.visibility="hidden";   
+    document.getElementById("BatsmenTop").style.visibility="hidden";
+    document.getElementById("BatSR").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="hidden";
+    document.getElementById("BatAVG").style.visibility="hidden"; 
+}
+function changeStats()
+{
+    document.getElementById("changeStatz").style.visibility="visible";
+    document.getElementById("changeResult").style.visibility="hidden";
+    document.getElementById("changeFixture").style.visibility="hidden";
+    document.getElementById("10v").style.visibility="hidden";
+    document.getElementById("5v").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="hidden";
+    document.getElementById("TopWicket").style.visibility="hidden";   
+    document.getElementById("BatsmenTop").style.visibility="hidden";
+    document.getElementById("BatSR").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="hidden";
+    document.getElementById("BatAVG").style.visibility="hidden"; }
+function topRun()
+{
+    document.getElementById("changeResult").style.visibility="hidden";
+    document.getElementById("changeFixture").style.visibility="hidden";
+    document.getElementById("BatsmenTop").style.visibility="visible";
+    document.getElementById("BatSR").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="hidden";
+    document.getElementById("BatAVG").style.visibility="hidden";
+    document.getElementById("5v").style.visibility="hidden";
+    document.getElementById("10v").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="hidden";
+    document.getElementById("TopWicket").style.visibility="hidden";    
+
+}
+function highScore()
+{
+    document.getElementById("changeResult").style.visibility="hidden";
+    document.getElementById("changeFixture").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="visible";
+    document.getElementById("BatsmenTop").style.visibility="hidden";
+    document.getElementById("BatSR").style.visibility="hidden";
+    document.getElementById("BatAVG").style.visibility="hidden";
+    document.getElementById("5v").style.visibility="hidden";
+    document.getElementById("10v").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="hidden";
+    document.getElementById("TopWicket").style.visibility="hidden";    
+}
+function batSR()
+{
+    document.getElementById("changeResult").style.visibility="hidden";
+    document.getElementById("changeFixture").style.visibility="hidden";
+    document.getElementById("BatSR").style.visibility="visible";
+    document.getElementById("BatAVG").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="hidden";
+    document.getElementById("BatsmenTop").style.visibility="hidden";
+    document.getElementById("5v").style.visibility="hidden";
+    document.getElementById("10v").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="hidden";
+    document.getElementById("TopWicket").style.visibility="hidden";    
+
+}
+function batAVG()
+{
+    document.getElementById("changeResult").style.visibility="hidden";
+    document.getElementById("changeFixture").style.visibility="hidden";
+    document.getElementById("BatAVG").style.visibility="visible";
+    document.getElementById("BatSR").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="hidden";
+    document.getElementById("BatsmenTop").style.visibility="hidden";
+    document.getElementById("5v").style.visibility="hidden";
+    document.getElementById("10v").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="hidden";
+    document.getElementById("TopWicket").style.visibility="hidden";  
+}
+function TopWic()
+{
+    document.getElementById("changeResult").style.visibility="hidden";
+    document.getElementById("changeFixture").style.visibility="hidden";
+    document.getElementById("TopWicket").style.visibility="visible";    
+    document.getElementById("BatsmenTop").style.visibility="hidden";
+    document.getElementById("BatSR").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="hidden";
+    document.getElementById("BatAVG").style.visibility="hidden";
+    document.getElementById("5v").style.visibility="hidden";
+    document.getElementById("10v").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="hidden";
+}
+function Eco()
+{
+    document.getElementById("changeResult").style.visibility="hidden";
+    document.getElementById("changeFixture").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="visible";
+    document.getElementById("TopWicket").style.visibility="hidden"; 
+    document.getElementById("5v").style.visibility="hidden";
+    document.getElementById("10v").style.visibility="hidden";   
+    document.getElementById("BatsmenTop").style.visibility="hidden";
+    document.getElementById("BatSR").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="hidden";
+    document.getElementById("BatAVG").style.visibility="hidden";
+
+}
+function FiveW()
+{
+    document.getElementById("changeResult").style.visibility="hidden";
+    document.getElementById("changeFixture").style.visibility="hidden";
+    document.getElementById("5v").style.visibility="visible";
+    document.getElementById("10v").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="hidden";
+    document.getElementById("TopWicket").style.visibility="hidden";  
+    document.getElementById("BatsmenTop").style.visibility="hidden";
+    document.getElementById("BatSR").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="hidden";
+    document.getElementById("BatAVG").style.visibility="hidden";  
+
+}
+function TenW()
+{
+    document.getElementById("changeResult").style.visibility="hidden";
+    document.getElementById("changeFixture").style.visibility="hidden";
+    document.getElementById("10v").style.visibility="visible";
+    document.getElementById("5v").style.visibility="hidden";
+    document.getElementById("ECO").style.visibility="hidden";
+    document.getElementById("TopWicket").style.visibility="hidden";   
+    document.getElementById("BatsmenTop").style.visibility="hidden";
+    document.getElementById("BatSR").style.visibility="hidden";
+    document.getElementById("BatsmenHighScore").style.visibility="hidden";
+    document.getElementById("BatAVG").style.visibility="hidden"; 
+}
+
+function slide()
+{
+    
+}
+
